5dfb14f062ce39b749d921ac926a595a4a9482e0
Andre B Farias <andre.dsbf@gmail.com>
2023-11-17 12:00:00 +0000

    v2

--- ui/base_tab.py
+++ ui/base_tab.py
@@ -1,24 +1,35 @@
-import tkinter as tk
-from tkinter import ttk, filedialog, messagebox
+#1
+# import tkinter as tk # LINHA ANTIGA COMENTADA
+# from tkinter import ttk, filedialog, messagebox # LINHA ANTIGA COMENTADA
+import customtkinter as ctk
+from tkinter import filedialog, messagebox
 import pandas as pd
 import os
 
-class BaseTab(ttk.Frame):
+#2
+# class BaseTab(ttk.Frame): # LINHA ANTIGA COMENTADA
+class BaseTab(ctk.CTkFrame):
     def __init__(self, master, app_instance):
-        super().__init__(master)
+        super().__init__(master, fg_color="transparent") # O fg_color='transparent' faz com que a aba assuma a cor do frame de conteúdo
         self.app = app_instance
         self.df = None
         self.filepath = None
-        self.pack(fill=tk.BOTH, expand=True)
+        # self.pack(fill=tk.BOTH, expand=True) # LINHA ANTIGA COMENTADA - O layout agora é controlado pelo grid na classe principal
 
+#4
     def selecionar_arquivo(self, label_widget, target_path_attr='filepath'):
+        # Utiliza o caminho de importação padrão se estiver definido nas configurações
+        initial_dir = self.app.config_manager.get("default_import_path") or os.path.expanduser("~")
+        
         filepath = filedialog.askopenfilename(
             title="Selecionar Arquivo",
-            filetypes=[("Excel files", "*.xlsx *.xls"), ("CSV files", "*.csv"), ("All files", "*.*")]
+            filetypes=[("Excel files", "*.xlsx *.xls"), ("CSV files", "*.csv"), ("All files", "*.*")],
+            initialdir=initial_dir
         )
         if filepath:
             setattr(self, target_path_attr, filepath)
-            label_widget.config(text=os.path.basename(filepath))
+            # Assumindo que label_widget agora é um ctk.CTkLabel, o método de config é .configure()
+            label_widget.configure(text=os.path.basename(filepath))
             self.app.log(f"Arquivo selecionado: {filepath}")
             return True
         return False
@@ -29,8 +40,10 @@ class BaseTab(ttk.Frame):
         
         try:
             self.app.log(f"Carregando {os.path.basename(file_path)}...")
+            # Usa o separador de CSV das configurações
+            csv_separator = self.app.config_manager.get("csv_separator") or ','
             if file_path.lower().endswith('.csv'):
-                df = pd.read_csv(file_path)
+                df = pd.read_csv(file_path, sep=csv_separator)
             else:
                 df = pd.read_excel(file_path)
             self.app.log("Arquivo carregado com sucesso.")
@@ -45,10 +58,13 @@ class BaseTab(ttk.Frame):
             messagebox.showwarning("Aviso", "Não há dados para salvar.")
             return False
 
-        initial_filename = "UNIFICADO" if is_unifier else f"processado_{os.path.basename(self.filepath or 'arquivo')}"
+        # Utiliza o caminho de exportação padrão se estiver definido
+        initial_dir = self.app.config_manager.get("default_export_path") or os.path.expanduser("~")
+        initial_filename = "UNIFICADO" if is_unifier else f"processado_{os.path.splitext(os.path.basename(self.filepath or 'arquivo'))[0]}"
 
         filepath = filedialog.asksaveasfilename(
             title="Salvar Arquivo Como",
+            initialdir=initial_dir,
             initialfile=initial_filename,
             defaultextension=".xlsx",
             filetypes=[("Excel files", "*.xlsx"), ("CSV files", "*.csv")]
@@ -58,12 +74,14 @@ class BaseTab(ttk.Frame):
             return False
 
         try:
+            csv_separator = self.app.config_manager.get("csv_separator") or ','
             if filepath.lower().endswith('.csv'):
-                df_to_save.to_csv(filepath, index=False, encoding='utf-8-sig')
+                df_to_save.to_csv(filepath, index=False, encoding='utf-8-sig', sep=csv_separator)
             else:
                 df_to_save.to_excel(filepath, index=False)
             
             self.app.log(f"Arquivo salvo com sucesso em {filepath}")
+            messagebox.showinfo("Sucesso", f"Arquivo salvo com sucesso em:\n{filepath}")
             return True
         except Exception as e:
             messagebox.showerror("Erro ao Salvar", f"Não foi possível salvar o arquivo:\n{e}")
@@ -72,12 +90,11 @@ class BaseTab(ttk.Frame):
             
     def salvar_dataframe_por_tipo(self, df_to_save, filepath, file_extension):
         try:
+            csv_separator = self.app.config_manager.get("csv_separator") or ','
             if file_extension == 'csv':
-                df_to_save.to_csv(filepath, index=False, encoding='utf-8-sig')
+                df_to_save.to_csv(filepath, index=False, encoding='utf-8-sig', sep=csv_separator)
             else:
                 df_to_save.to_excel(filepath, index=False)
             self.app.log(f" -> Arquivo salvo: {os.path.basename(filepath)}")
         except Exception as e:
             self.app.log(f" -> ERRO ao salvar {os.path.basename(filepath)}: {e}")
-
-
